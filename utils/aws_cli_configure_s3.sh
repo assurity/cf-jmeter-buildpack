948d11aea68abfb98b036325ebcc4e6000e7842d
matthewt-assurity <matthew.tansley@assurity.co.nz>
2025-03-13 16:07:47 +1300

    Added commands to configure aws creds using s3 service instance creds

--- utils/aws_cli_configure_s3.sh
+++ utils/aws_cli_configure_s3.sh
@@ -18,14 +18,13 @@ else
 fi
 
 # Configure the AWS CLI to use the S3 credentials from VCAP_SERVICES.
-# Assumes the app you are deploying is bound to an S3 service instance.
-# echo "-----> Set S3 service instance credentials"
-# AWS_ACCESS_KEY_ID="$(echo $VCAP_SERVICES | jq -r '.[csb-aws-s3-bucket][0].credentials.access_key_id')"
-# AWS_SECRET_ACCESS_KEY="$(echo $VCAP_SERVICES | jq -r '.[csb-aws-s3-bucket][0].credentials.secret_access_key')"
-# AWS_DEFAULT_REGION="$(echo $VCAP_SERVICES | jq -r '.[csb-aws-s3-bucket][0].credentials.region')"
+echo "-----> Set S3 service instance credentials"
+AWS_ACCESS_KEY_ID="$(echo $VCAP_SERVICES | jq -r '.["csb-aws-s3-bucket"][0].credentials.access_key_id')"
+AWS_SECRET_ACCESS_KEY="$(echo $VCAP_SERVICES | jq -r '.["csb-aws-s3-bucket"][0].credentials.secret_access_key')"
+AWS_DEFAULT_REGION="$(echo $VCAP_SERVICES | jq -r '.["csb-aws-s3-bucket"][0].credentials.region')"
 
-# # Export AWS creds
-# export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
-# export AWS_SECRET_ACCESS_KEY_ID=$AWS_SECRET_ACCESS_KEY
-# export AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION
-# aws configure list
+# Export AWS creds
+export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY_ID=$AWS_SECRET_ACCESS_KEY
+export AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION
+aws configure list
